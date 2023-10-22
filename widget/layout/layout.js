741e648db8d65f6ad67e9ed65b11bf7c36736526
CHHUT Pech <chhutpich4@gmail.com>
2023-10-22 11:37:30 +0700

    add

--- widget/layout/layout.js
+++ widget/layout/layout.js
@@ -1,52 +1,52 @@
-const menuBtn = document.querySelector('.app-nav-bar-right-btn')
-const appSlider = document.querySelector('.app-slider')
-const appSliderMenu = document.querySelector('.app-slider-menu')
-const appSliderMenuItems = document.querySelectorAll('.app-slider-menu-item');
-const backBtn = document.querySelectorAll('.back-btn');
-const searchBtn = document.querySelector('.search-btn')
-const closeSearchBtn = document.querySelector('.close-btn')
-const appNavBarAction = document.querySelector('.app-nav-bar-action')
+function layoutFunction() {
+    const menuBtn = document.querySelector('.app-nav-bar-right-btn')
+    const appSlider = document.querySelector('.app-slider')
+    const appSliderMenu = document.querySelector('.app-slider-menu')
+    const appSliderMenuItems = document.querySelectorAll('.app-slider-menu-item');
+    const backBtn = document.querySelectorAll('.back-btn');
+    const searchBtn = document.querySelector('.search-btn')
+    const closeSearchBtn = document.querySelector('.close-btn')
+    const appNavBarAction = document.querySelector('.app-nav-bar-action')
 
-menuBtn.addEventListener('click', () => {
-    appSlider.classList.toggle('open-slider')
-    menuBtn.classList.toggle('fa-bars')
-    menuBtn.classList.toggle('fa-xmark')
-})
-
-searchBtn.addEventListener('click', () => {
-    appNavBarAction.classList.toggle('open-search-bar')
-    if (appSlider.classList.contains('open-slider')) {
+    menuBtn.addEventListener('click', () => {
         appSlider.classList.toggle('open-slider')
         menuBtn.classList.toggle('fa-bars')
         menuBtn.classList.toggle('fa-xmark')
-    }
-})
+    })
 
-closeSearchBtn.addEventListener('click', () => {
-    appNavBarAction.classList.toggle('open-search-bar')
-})
+    searchBtn.addEventListener('click', () => {
+        appNavBarAction.classList.toggle('open-search-bar')
+        if (appSlider.classList.contains('open-slider')) {
+            appSlider.classList.toggle('open-slider')
+            menuBtn.classList.toggle('fa-bars')
+            menuBtn.classList.toggle('fa-xmark')
+        }
+    })
 
-appSliderMenuItems.forEach(item => {
-    item.addEventListener('click', (event)=> {
-        appSliderMenuItems.forEach(otherItem => {
-            if(item != otherItem){
-                otherItem.classList.remove('open-slider-item');
-            }
+    closeSearchBtn.addEventListener('click', () => {
+        appNavBarAction.classList.toggle('open-search-bar')
+    })
+
+    appSliderMenuItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            appSliderMenuItems.forEach(otherItem => {
+                if (item != otherItem) {
+                    otherItem.classList.remove('open-slider-item');
+                }
+            });
+
+            item.classList.toggle('open-slider-item');
         });
-
-        item.classList.toggle('open-slider-item');
     });
-});
 
-backBtn.forEach(item => {
-    item.addEventListener('click', (event)=> {
-        event.stopPropagation();
-        appSliderMenuItems.forEach(otherItem => {
-            otherItem.classList.remove('open-slider-item')
+    backBtn.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.stopPropagation();
+            appSliderMenuItems.forEach(otherItem => {
+                otherItem.classList.remove('open-slider-item')
+            });
         });
     });
-});
 
 
-
-
+}
