c3599f787c9193e8a515aa49144f56d997756e6c
CHHUT Pech <chhutpich4@gmail.com>
2023-11-18 20:54:09 +0700

    add

---
+++ page/account/login/login.js
@@ -0,0 +1,6 @@
+function addContent(){
+    var appContent = document.querySelector('.app-content');
+    var addContent = document.querySelector('.add-content');
+    appContent.innerHTML = addContent.innerHTML;
+    addContent.innerHTML = ''
+}
