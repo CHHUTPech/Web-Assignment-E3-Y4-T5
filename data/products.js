c5274612a771fa1ab8d9ed26754ac30f2c726bb2
CHHUT Pech <chhutpich4@gmail.com>
2023-11-23 17:51:49 +0700

    add

---
+++ data/products.js
@@ -0,0 +1,27 @@
+const products = [
+    {
+        id: '1',
+        name: 'Louise Slide Sandal',
+        price: 120.69,
+        d_price: 110.99,
+        img: '/assets/images/mlouye-helix-denim-2.webp'
+    }, {
+        id: '2',
+        name: 'Otti Combat Boot',
+        price: 120.69,
+        d_price: 110.99,
+        img: '/assets/images/mlouye-helix-denim-2.webp'
+    }, {
+        id: '3',
+        name: 'Thelma Sandal',
+        price: 120.69,
+        d_price: 110.99,
+        img: '/assets/images/mlouye-helix-denim-2.webp'
+    }, {
+        id: '4',
+        name: 'Pleated Heel Boot',
+        price: 120.69,
+        d_price: 110.99,
+        img: '/assets/images/mlouye-helix-denim-2.webp'
+    },
+]
